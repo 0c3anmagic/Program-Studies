fdba595f45ede43268eb92e22f44402e856c3ade
drmack112 <69864665+drmack112@users.noreply.github.com>
2022-11-18 09:16:08 -0500

    Update Project
    Uploaded and combined Owen's and Rushi's work into the project

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
-$(document).ready(function() {
-    const modalBtn = document.querySelector('.modal-btn');
-    const modalBtnOne = document.querySelector('.modal-btn-one');
-    const modalBtnTwo = document.querySelector('.modal-btn-two');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    const modal = document.querySelector('.modal-overlay');
-    const modalOne = document.querySelector('.modal-overlay-one');
-    const modalTwo = document.querySelector('.modal-overlay-two');
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-    const closeBtn = document.querySelector('.close-btn');
-    const closeBtnOne = document.querySelector('.close-btn-one');
-    const closeBtnTwo = document.querySelector('.close-btn-two');
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-    modalBtn.addEventListener('click', function() {
-        modal.classList.add("open-modal")
-    })
-    modalBtnOne.addEventListener('click', function() {
-        modalOne.classList.add("open-modal-one")
-    })
-    modalBtnTwo.addEventListener('click', function() {
-        modalTwo.classList.add("open-modal-two")
-    })
-
-    closeBtn.addEventListener('click', function() {
-        modal.classList.remove("open-modal")
-    })
-    closeBtnOne.addEventListener('click', function() {
-        modalOne.classList.remove("open-modal-one")
-    })
-    closeBtnTwo.addEventListener('click', function() {
-        modalTwo.classList.remove("open-modal-two")
-    })
-
-    modal.addEventListener('click', function() {
-        modal.classList.remove("open-modal")
-    })
-    modalOne.addEventListener('click', function() {
-        modalOne.classList.remove("open-modal-one")
-    })
-    modalTwo.addEventListener('click', function() {
-        modalTwo.classList.remove("open-modal-two")
-    })
-});
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
